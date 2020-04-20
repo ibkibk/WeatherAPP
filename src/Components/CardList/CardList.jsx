f1489d39d67828718fb36a59baee595770bfcb82
Ibrahim Kurhan <ikurhan@outlook.com>
2020-04-21 01:30:57 +0300

    temperature converted to celsius

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -9,32 +9,27 @@ export default class CardList extends Component {
       { city_id: 4749005 },
       { city_id: 4219762 },
     ],
-    filteredcities: [],
+    threecities: [],
   };
 
   componentDidMount() {
-    // WebAuthnAssertion.temp
-    // const kelvin = 273;
     let string = "";
     this.state.cityName.forEach((city) => (string += `${city.city_id},`));
     fetch(
-      `https://api.openweathermap.org/data/2.5/group?id=${string}&appid=0f33f5c78acf44e7d38b5f6706f6f59d`
+      `https://api.openweathermap.org/data/2.5/group?id=${string}&units=metric&appid=0f33f5c78acf44e7d38b5f6706f6f59d`
     )
       .then((response) => response.json())
-      // .then(function(data){
-      //    cities.data.celcious = Math.floor(data.main.temp - 273)
-      // })
+
       .then((cities) => {
-        this.setState({ filteredcities: cities["list"] });
+        this.setState({ threecities: cities["list"] });
       });
-    // .then(celcious => celcious === kalvin - 275.15)
   }
   render() {
-    console.log(this.state.filteredcities);
+    console.log(this.state.threecities);
     return (
       <>
         <section className={styles.card}>
-          {this.state.filteredcities.map((city, index) => (
+          {this.state.threecities.map((city, index) => (
             <Cards city={city} key={index} />
           ))}
         </section>
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,28 +1,18 @@
 import React, { Component } from "react";
 import style from "./Cards.module.scss";
 
-
 export default class Cards extends Component {
-    render() {
-        return (
-            <div className={style.main}>
-                <div className={style.box}>
-                    <h1>{this.props.city.name}</h1>
-                    <p>{this.props.city.main.temp}</p>
-                    <h2> {this.props.city.main.humidity}</h2>
-                    <h2>{this.props.city.main.temp_min}</h2>
-                    <p>{this.props.city.main.temp_max}</p> 
-                
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={style.main}>
+        <div className={style.box}>
+          <h1>{this.props.city.name}</h1>
+          <p>{Math.round(this.props.city.main.temp)}°c</p>
+          <h2> {this.props.city.main.humidity}</h2>
+          <h2>{Math.round(this.props.city.main.temp_min)}°c</h2>
+          <p>{Math.round(this.props.city.main.temp_max)}°c</p>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
-
-
